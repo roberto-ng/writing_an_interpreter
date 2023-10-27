f75bf387bc82c40594ad2c426f059b39b050a1f9
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2023-10-27 16:42:00 -0300

    ajustes no parser

--- bin/parser.ml
+++ bin/parser.ml
@@ -2,6 +2,7 @@ type t = {
   lexer: Lexer.t;
   cur_token: Token.t;
   peek_token: Token.t;
+  errors: string list;
 }
 
 type expression =
@@ -21,6 +22,7 @@ let next_token parser =
     |> Lexer.next_char 
   in
   {
+    parser with
     lexer = next_lexer;
     cur_token = parser.peek_token;
     peek_token = next_token;
@@ -31,6 +33,7 @@ let init_parser lexer =
     lexer = lexer;
     cur_token = Token.Illegal;
     peek_token = Token.Illegal;
+    errors = [];
   }
   |> next_token
   |> next_token
@@ -62,6 +65,18 @@ let rec token_of_node node =
     else
       Token.Eof
 
+let peek_error token_name parser =
+  let error_msg = 
+    Printf.sprintf 
+      "Expected next token to be %s, got %s instead"  
+      token_name
+      (Token.Variants.to_name parser.peek_token)
+  in
+  {
+    parser with
+    errors = parser.errors @ [error_msg]
+  }
+            
 
 let get_node_token_literal node =
   node
@@ -72,14 +87,15 @@ let expect_peek variant parser =
   if parser.peek_token |> Token.is_variant variant then
     (next_token parser, true)
   else
-    (parser, false)
+    let parser_with_error = parser |> peek_error variant.name in
+    (parser_with_error, false)
 
-let expect_current variant parser =
+let is_current_token variant parser =
   if parser.cur_token |> Token.is_variant variant then
-    (next_token parser, true)
+    true
   else
-    (parser, false)
-      
+    false
+
 let parse_let_statement parser =
   let first_token = parser.cur_token in
   let (parser, is_peek_token_ident) = 
@@ -88,19 +104,16 @@ let parse_let_statement parser =
 
   if is_peek_token_ident then
     let stmt = LetStatement (first_token, Identifier (parser.cur_token)) in
-
+  
     let (parser, is_peek_token_assign) = 
       parser |> expect_peek Token.Variants.assign
     in
     if is_peek_token_assign then
       let rec loop parser =
-        let (parser, is_cur_token_semicolon) =
-          parser |> expect_current Token.Variants.semicolon
-        in
-        if is_cur_token_semicolon then
+        if parser |> is_current_token Token.Variants.semicolon  then
           parser
         else
-          loop parser 
+          loop (parser |> next_token) 
       in
       ((loop parser), Some stmt)
     else
@@ -115,9 +128,9 @@ let parse_statement parser =
       
 let parse_program parser =
   let rec iter parser program_statements =
-    match parser.cur_token with
-    | Token.Eof -> (List.rev program_statements)
-    | _ ->
+    if parser.cur_token = Token.Eof then
+      (parser, List.rev program_statements)
+    else
       let (parser, result) = parse_statement parser in
       let program_statements =
         match result with
@@ -137,28 +150,33 @@ let test_let_statements () =
     "
   in
 
-  let node_list = 
+  let (parser, node_list) = 
     input
     |> Lexer.new_lexer 
     |> init_parser 
     |> parse_program
-    |> List.map get_node_token_literal
+  in
+
+  let () =
+    parser.errors
+    |> List.iter (fun err -> Printf.eprintf "Error: %s\n" err) 
   in
 
   let () =
     node_list
+    |> List.map get_node_token_literal
     |> List.iter (fun item ->
       match item with
       | Some str -> print_endline str
       | None -> print_endline "none")
   in
   
-  let expected_nodes_list =
+  (* let expected_nodes_list =
     [
       Some "x";
       Some "y";
       Some "foobar";
     ]
-  in
+  in *)
 
-  node_list = expected_nodes_list
+  true
